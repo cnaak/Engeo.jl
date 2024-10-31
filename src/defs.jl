a8b83d6c222b1d98965ec703aa59d942df8841f7
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-10-31 16:08:01 -0300

    UPD the src/defs.jl

--- src/defs.jl
+++ src/defs.jl
@@ -6,5 +6,18 @@ struct Engine <: Any
     rv::Float64         # =Vmax/Vmin
     z::Int64            # #cyl.
     Vd::Unitful.Volume  # Swiped volume
+    # Internal Constructor
+    Engine(rsd::AbstractFloat,
+           rlr::AbstractFloat,
+           rv::AbstractFloat,
+           zz::Integer,
+           vd::Unitful.Volume) = begin
+        @assert rsd > 0.0
+        @assert rlr > 0.0
+        @assert rv  > 0.0
+        @assert zz  >=  1
+        @assert vd  > 0.0 * u"m^3"
+        return new(rsd, rlr, rv, zz, uconvert(u"m^3", vd))
+    end
 end
 
